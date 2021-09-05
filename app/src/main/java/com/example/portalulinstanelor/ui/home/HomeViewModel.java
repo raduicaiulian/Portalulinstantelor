88f81358beebb9fa095686a2180d537037fbc107
raduicaiulian <raduicaiulian@gmail.com>
2021-09-05 04:04:22 +0300

    added logo+dashboard

--- app/src/main/java/com/example/portalulinstanelor/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/portalulinstanelor/ui/home/HomeViewModel.java
@@ -1,16 +1,24 @@
 package com.example.portalulinstanelor.ui.home;
 
+import static com.android.volley.toolbox.Volley.newRequestQueue;
+
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import java.io.IOException;
 
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public HomeViewModel() throws IOException {
         mText = new MutableLiveData<>();
+
         mText.setValue("This is home fragment");
+        Log.d("HomeView", "Test message");
+
     }
 
     public LiveData<String> getText() {
